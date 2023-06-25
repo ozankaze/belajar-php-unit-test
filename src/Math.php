b1c118eca7777446dac84967a5cd9b5534d7a333
ozankaze <ozankaze101@gmail.com>
2023-06-25 10:21:52 +0700

    Data Provider

---
+++ src/Math.php
@@ -0,0 +1,19 @@
+<?php
+
+// 45 
+
+namespace Programmerzamannow\BelajarPhpUnitTest;
+
+class Math
+{
+
+    public static function sum(array $values): int
+    {
+        $total = 0;
+        foreach ($values as $value) {
+            $total += $value;
+        }
+        return $total;
+    }
+
+}
